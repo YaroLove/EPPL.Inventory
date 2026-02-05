f2129e2c89b3f212327a0baab178e2a64942997c
YaroLove <vasyaroslav.24@gmail.com>
2026-02-04 20:49:56 -0800

    Refactor SearchBar component to handle search input

--- src/components/Items/SearchBar.jsx
+++ src/components/Items/SearchBar.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { Input, Space } from 'antd';
+import React from 'react';
+import { Input } from 'antd';
 import { useDispatch, useSelector } from 'react-redux';
 import { setFilter } from './filterSlice';
 
@@ -8,19 +8,27 @@ const { Search } = Input;
 export const SearchBar = () => {
   const searchInput = useSelector((state) => state.filter.filter);
   const dispatch = useDispatch();
+
   const filter = (e) => {
     dispatch(setFilter(e.target.value));
   };
 
+  const onSearch = (value) => {
+    // Pressing Enter or clicking the search button dispatches the same action
+    dispatch(setFilter(value));
+  };
+
   return (
-    <div>
+    <div className="inventory-search-wrapper">
       <Search
         placeholder="Search inventory"
         allowClear
         className="inventory-search"
         style={{ width: 'min(520px, 70vw)', margin: '1rem 1rem' }}
         onChange={filter}
+        onSearch={onSearch}
         value={searchInput}
+        aria-label="Search inventory"
       />
     </div>
   );
